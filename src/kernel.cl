0c6230a3eb54d4832a5d0954141d7cd59156a6a5
Dries007 <admin@dries007.net>
2018-05-08 15:57:45 +0200

    update (kernels klaar)

--- src/kernel.cl
+++ src/kernel.cl
@@ -1,24 +1,42 @@
 /*
  * nBody problem OpenCL kernel
  */
-typedef struct body
-{
-    float3 pos;
-    float3 speed;
-} Body;
 
 #ifdef FOEFELMAGIC
 #define __global
 #define __kernel
+#define float3 struct {float x, y, z;}
+#define get_global_id(dim) dim
+#define sqrt(a) a
+#define CLK_GLOBAL_MEM_FENCE 0
+#define barrier(type) do {} while(0)
 #endif
 
-__kernel void kernel_step(unsigned long n, __global Body * data)
+typedef union
+{
+    float3 vec;
+    float arr[3];
+} float3_;
+
+typedef struct body
+{
+    float3 pos;
+    float3_ speed;
+} Body;
+
+/**
+ * Laptop Dries NVIDIA, 5000, avg: 0.051  s (avg 100 frames) 19.6 FPS
+ * Laptop Dries NVIDIA, 1000, avg: 0.0034 s (avg 100 frames) 294  FPS
+ * Laptop Dries INTEL,  5000, avg: 0.31   s (avg 100 frames)  3.2 FPS
+ * Laptop Dries INTEL,  1000, avg: 0.017  s (avg 100 frames)   59 FPS
+ */
+__kernel void kernel_step_float3(unsigned long n, __global Body * data)
 {
     const float SCALE = 50;
-    const float TIME_DELTA = .1;
+    const float TIME_DELTA = 1;
     const float GRAV_CONST = 1;
     const float MASS = 2;
-    const float MASS_GAV = GRAV_CONST * MASS * MASS;
+    const float MASS_GAV = (GRAV_CONST * MASS * MASS);
 
     const int body = get_global_id(0);
 
@@ -37,12 +55,117 @@ __kernel void kernel_step(unsigned long n, __global Body * data)
         float dist = sqrt(dist_all_sq.x + dist_all_sq.y + dist_all_sq.z);
 
         float3 acc = (-MASS_GAV * dist_all / (dist * dist * dist)) / MASS;
-        us->speed += acc * TIME_DELTA;
+        us->speed.vec += acc * TIME_DELTA;
     }
 
     barrier(CLK_GLOBAL_MEM_FENCE);
 
-    us->pos += us->speed * (TIME_DELTA / SCALE);
+    us->pos += us->speed.vec * (TIME_DELTA / SCALE);
 
     barrier(CLK_GLOBAL_MEM_FENCE);
+}
+
+/**
+ * Laptop Dries NVIDIA, 5000, avg: 0.051  s (avg 100 frames) 19.6 FPS
+ * Laptop Dries NVIDIA, 1000, avg: 0.0034 s (avg 100 frames)  294 FPS
+ * Laptop Dries INTEL,  5000, avg: 0.30   s (avg 100 frames)  3.3 FPS
+ * Laptop Dries INTEL,  1000, avg: 0.017  s (avg 100 frames)   59 FPS
+ */
+__kernel void kernel_step_float(unsigned long n, __global Body * data)
+{
+    const float SCALE = 50;
+    const float TIME_DELTA = 1;
+    const float GRAV_CONST = 1;
+    const float MASS = 2;
+    const float MASS_GAV = (GRAV_CONST * MASS * MASS);
+
+    const int body = get_global_id(0);
+
+    if (body >= n) return;
+
+    __global Body* us = &data[body];
+
+    for (int i = 0; i < n; i++)
+    {
+        if (i == body) continue;
+
+        __global const Body* them = &data[i];
+
+        float dist_x = (us->pos.x - them->pos.x) * SCALE;
+        float dist_y = (us->pos.y - them->pos.y) * SCALE;
+        float dist_z = (us->pos.z - them->pos.z) * SCALE;
+
+        float dist = sqrt(dist_x * dist_x + dist_y * dist_y + dist_z * dist_z);
+
+        float acc_x = (-MASS_GAV * dist_x / (dist * dist * dist)) / MASS;
+        float acc_y = (-MASS_GAV * dist_y / (dist * dist * dist)) / MASS;
+        float acc_z = (-MASS_GAV * dist_z / (dist * dist * dist)) / MASS;
+
+
+        us->speed.vec.x += acc_x * TIME_DELTA;
+        us->speed.vec.y += acc_y * TIME_DELTA;
+        us->speed.vec.z += acc_z * TIME_DELTA;
+    }
+
+    barrier(CLK_GLOBAL_MEM_FENCE);
+
+    us->pos.x += us->speed.vec.x * (TIME_DELTA / SCALE);
+    us->pos.y += us->speed.vec.y * (TIME_DELTA / SCALE);
+    us->pos.z += us->speed.vec.z * (TIME_DELTA / SCALE);
+}
+
+// Based on: http://suhorukov.blogspot.be/2011/12/opencl-11-atomic-operations-on-floating.html
+inline void AtomicAdd(volatile __global float *source, const float operand) {
+    union {
+        unsigned int intVal;
+        float floatVal;
+    } newVal;
+    union {
+        unsigned int intVal;
+        float floatVal;
+    } prevVal;
+    do {
+        prevVal.floatVal = *source;
+        newVal.floatVal = prevVal.floatVal + operand;
+    } while (atomic_cmpxchg((volatile __global unsigned int *)source, prevVal.intVal,
+                            newVal.intVal) != prevVal.intVal);
+}
+
+/**
+ * Laptop Dries INTEL,  1000, avg: 0.072 s (avg 100 frames)   14 FPS
+ * Laptop Dries INTEL,  5000, avg: 1.5   s (avg 100 frames)  0.7 FPS
+ * Laptop Dries NVIDIA, 1000, avg: 0.022 s (avg 100 frames)   45 FPS
+ * Laptop Dries NVIDIA, 5000, avg: 0.47  s (avg 100 frames)    2 FPS
+ */
+__kernel void kernel2_step_float3(unsigned long n, __global Body * data)
+{
+    const float SCALE = 50;
+    const float TIME_DELTA = 1;
+    const float GRAV_CONST = 1;
+    const float MASS = 2;
+    const float MASS_GAV = (GRAV_CONST * MASS * MASS);
+
+    const int body = get_global_id(0);
+
+    if (body >= n) return;
+
+    __global Body* us = &data[body];
+
+    const int i = get_global_id(1);
+
+    if (i >= n) return;
+
+    if (i == body) return;
+
+    __global const Body* them = &data[i];
+
+    float3 dist_all = (us->pos - them->pos) * SCALE;
+    float3 dist_all_sq = dist_all * dist_all;
+    float dist = sqrt(dist_all_sq.x + dist_all_sq.y + dist_all_sq.z);
+
+    float3 acc = (-MASS_GAV * dist_all / (dist * dist * dist)) / MASS;
+
+    AtomicAdd(&us->speed.arr[0], acc.x * TIME_DELTA);
+    AtomicAdd(&us->speed.arr[1], acc.y * TIME_DELTA);
+    AtomicAdd(&us->speed.arr[2], acc.z * TIME_DELTA);
 }
--- src/main.c
+++ src/main.c
@@ -7,6 +7,13 @@
 #include "lib/time_utils.h"
 #include "lib/renderer.h"
 
+enum MODE {
+    MODE_CPU,
+    MODE_FLOAT3,
+    MODE_FLOAT,
+    MODE_2D,
+};
+
 typedef struct body
 {
     cl_float3 pos;
@@ -22,6 +29,7 @@ typedef struct callback_data {
     float point_size;
     bool draw_lines;
     Vect4f line_color;
+    enum MODE mode;
 } CallbackData;
 
 Body* create_bodies(size_t n)
@@ -69,22 +77,144 @@ void draw(void* data)
     }
 }
 
+struct timespec diff(struct timespec start, struct timespec end)
+{
+    struct timespec temp;
+    if ((end.tv_nsec-start.tv_nsec)<0) {
+        temp.tv_sec = end.tv_sec-start.tv_sec-1;
+        temp.tv_nsec = 1000000000+end.tv_nsec-start.tv_nsec;
+    } else {
+        temp.tv_sec = end.tv_sec-start.tv_sec;
+        temp.tv_nsec = end.tv_nsec-start.tv_nsec;
+    }
+    return temp;
+}
+
+void kernel_cpu(CallbackData* cd)
+{
+    /**
+     * Laptop Dries CPU, 1000, avg: 0.038 s (avg 100 frames) 26.3 FPS
+     * Laptop Dries CPU, 5000, avg: 0.93  s (avg 100 frames)    1 FPS
+     */
+    const float delta_time = 1.f;
+    // const float grav_constant = 6.67428e-11;
+    const float grav_constant = 1;
+    const float mass_of_sun = 2;
+    const float mass_grav = grav_constant * mass_of_sun * mass_of_sun;
+    const float distance_to_nearest_star = 50;
+
+    for (int i = 0; i < cd->n; ++i)
+    {
+        for (int j = 0; j < cd->n; ++j)
+        {
+            if (i == j) continue;
+
+            cl_float3 pos_a = cd->bodies[i].pos;
+            cl_float3 pos_b = cd->bodies[j].pos;
+
+            float dist_x = (pos_a.s[0] - pos_b.s[0]) * distance_to_nearest_star;
+            float dist_y = (pos_a.s[1] - pos_b.s[1]) * distance_to_nearest_star;
+            float dist_z = (pos_a.s[2] - pos_b.s[2]) * distance_to_nearest_star;
+
+
+            float distance = sqrt(
+                    dist_x * dist_x +
+                    dist_y * dist_y +
+                    dist_z * dist_z);
+
+            float force_x = -mass_grav * dist_x / (distance * distance * distance);
+            float force_y = -mass_grav * dist_y / (distance * distance * distance);
+            float force_z = -mass_grav * dist_z / (distance * distance * distance);
+
+            float acc_x = force_x / mass_of_sun;
+            float acc_y = force_y / mass_of_sun;
+            float acc_z = force_z / mass_of_sun;
+
+            cd->bodies[i].speed.s[0] += acc_x * delta_time;
+            cd->bodies[i].speed.s[1] += acc_y * delta_time;
+            cd->bodies[i].speed.s[2] += acc_z * delta_time;
+        }
+    }
+
+    for (int i = 0; i < cd->n; ++i)
+    {
+        cd->bodies[i].pos.s[0] += (cd->bodies[i].speed.s[0] * delta_time) / distance_to_nearest_star;
+        cd->bodies[i].pos.s[1] += (cd->bodies[i].speed.s[1] * delta_time) / distance_to_nearest_star;
+        cd->bodies[i].pos.s[2] += (cd->bodies[i].speed.s[2] * delta_time) / distance_to_nearest_star;
+    }
+}
+
+#define AVG_MAX 100
 void step(void* data)
 {
-//    time_measure_start("step");
+    static struct timespec avg[AVG_MAX];
+    static int time_index = 0;
+
+    struct timespec time1, time2;
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time1);
+
     CallbackData* cd = data;
-    ocl_err(clEnqueueNDRangeKernel(g_command_queue, cd->kernel, 1, NULL, &cd->n, NULL, 0, NULL, NULL));
-    ocl_err(clFinish(g_command_queue));
-    ocl_err(clEnqueueReadBuffer(g_command_queue, cd->dev_bodies, CL_TRUE, 0, sizeof(Body) * cd->n, cd->bodies, 0, NULL, NULL));
-    ocl_err(clFinish(g_command_queue));
-//    time_measure_stop_and_print("step");
+
+    switch (cd->mode)
+    {
+        case MODE_CPU:
+            kernel_cpu(cd);
+        break;
+        case MODE_FLOAT3:
+        case MODE_FLOAT:
+            ocl_err(clEnqueueNDRangeKernel(g_command_queue, cd->kernel, 1, NULL, &cd->n, NULL, 0, NULL, NULL)); //1D
+            ocl_err(clFinish(g_command_queue));
+            ocl_err(clEnqueueReadBuffer(g_command_queue, cd->dev_bodies, CL_TRUE, 0, sizeof(Body) * cd->n, cd->bodies, 0, NULL, NULL));
+            ocl_err(clFinish(g_command_queue));
+            break;
+        case MODE_2D:
+        {
+            size_t size[] = {cd->n, cd->n};
+            ocl_err(clEnqueueWriteBuffer(g_command_queue, cd->dev_bodies, CL_TRUE, 0, sizeof(Body) * cd->n, cd->bodies, 0, NULL, NULL));
+            ocl_err(clFinish(g_command_queue));
+            ocl_err(clEnqueueNDRangeKernel(g_command_queue, cd->kernel, 2, NULL, size, NULL, 0, NULL, NULL)); //2D
+            ocl_err(clFinish(g_command_queue));
+            ocl_err(clEnqueueReadBuffer(g_command_queue, cd->dev_bodies, CL_TRUE, 0, sizeof(Body) * cd->n, cd->bodies, 0, NULL, NULL));
+            ocl_err(clFinish(g_command_queue));
+            const float SCALE = 50;
+            const float TIME_DELTA = 1;
+            for (int i = 0; i < cd->n; ++i)
+            {
+                cd->bodies[i].pos.s[0] += (cd->bodies[i].speed.s[0] * TIME_DELTA) / SCALE;
+                cd->bodies[i].pos.s[1] += (cd->bodies[i].speed.s[1] * TIME_DELTA) / SCALE;
+                cd->bodies[i].pos.s[2] += (cd->bodies[i].speed.s[2] * TIME_DELTA) / SCALE;
+            }
+        }
+            break;
+        default:
+            abort();
+    }
+
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time2);
+    avg[time_index] = diff(time1, time2);
+    time_index ++;
+    if (time_index == AVG_MAX)
+    {
+        time_index = 0;
+        double sum = 0;
+        for (int i = 0; i < AVG_MAX; i++) {
+            sum += avg[i].tv_sec + avg[i].tv_nsec / 1000000000.0;
+        }
+        sum /= AVG_MAX;
+        printf("AVG: %lf\n", sum);
+    }
 }
 
 int main(int argc, char ** argv)
 {
-    if (argc <= 1)
+    if (argc <= 2)
     {
-        printf("%s <number of bodies>\n", argv[0]);
+        printf("%s <number of bodies> <mode>\n", argv[0]);
+        printf("Mode must be one of:\n");
+        printf("- CPU\n");
+        printf("- FLOAT\n");
+        printf("- FLOAT3\n");
+        printf("- 2D\n");
         return -1;
     }
     errno = 0;
@@ -94,11 +224,24 @@ int main(int argc, char ** argv)
         printf("'%s' was not converted to a proper number. (%ld)\n", argv[1], n);
         return -1;
     }
+    enum MODE mode;
+    if (strcmp(argv[2], "CPU") == 0 || strcmp(argv[2], "cpu") == 0) mode = MODE_CPU;
+    else if (strcmp(argv[2], "FLOAT3") == 0 || strcmp(argv[2], "float3") == 0) mode = MODE_FLOAT3;
+    else if (strcmp(argv[2], "FLOAT") == 0 || strcmp(argv[2], "float") == 0) mode = MODE_FLOAT;
+    else if (strcmp(argv[2], "2D") == 0 || strcmp(argv[2], "2d") == 0) mode = MODE_2D;
+    else
+    {
+        printf("'%s' was not converted to a mode.\n", argv[1]);
+        return -1;
+    }
 
-    cl_platform_id platform = ocl_select_platform();
-    cl_device_id device = ocl_select_device(platform);
-    init_ocl(device);
-    create_program("kernel.cl", "");
+    if (mode != MODE_CPU)
+    {
+        cl_platform_id platform = ocl_select_platform();
+        cl_device_id device = ocl_select_device(platform);
+        init_ocl(device);
+        create_program("kernel.cl", "");
+    }
 
     char* title = malloc(1 + (size_t) snprintf(NULL, 0, "nBody problem - %ld bodies", n));
     sprintf(title, "nBody problem - %ld bodies", n);
@@ -107,22 +250,43 @@ int main(int argc, char ** argv)
     srand((unsigned int) time(NULL));
     Body* bodies = create_bodies((size_t) n);
 
-    cl_int error;
-
-    // Data buffer on GPU (RW)
-    cl_mem dev_bodies = clCreateBuffer(g_context, CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR, sizeof(Body) * n, bodies, &error);
-    ocl_err(error);
-    ocl_err(clFinish(g_command_queue));
-
     // Create kernel
-    cl_kernel kernel = clCreateKernel(g_program, "kernel_step", &error);
-    ocl_err(error);
-    ocl_err(clFinish(g_command_queue));
-    // Set kernel arguments
-    cl_uint arg_num = 0;
-    ocl_err(clSetKernelArg(kernel, arg_num++, sizeof(n), &n));
-    ocl_err(clSetKernelArg(kernel, arg_num++, sizeof(dev_bodies), &dev_bodies));
-    ocl_err(clFinish(g_command_queue));
+    cl_int error;
+    cl_kernel kernel;
+    cl_mem dev_bodies;
+    switch (mode)
+    {
+        case MODE_CPU:
+            kernel = NULL;
+            dev_bodies = NULL;
+            break;
+        case MODE_FLOAT3:
+            kernel = clCreateKernel(g_program, "kernel_step_float3", &error);
+            break;
+        case MODE_FLOAT:
+            kernel = clCreateKernel(g_program, "kernel_step_float", &error);
+            break;
+        case MODE_2D:
+            kernel = clCreateKernel(g_program, "kernel2_step_float3", &error);
+            break;
+        default:
+            abort();
+    }
+    if (kernel != NULL)
+    {
+        ocl_err(error);
+        ocl_err(clFinish(g_command_queue));
+        // Data buffer on GPU (RW)
+        dev_bodies = clCreateBuffer(g_context, CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR, sizeof(Body) * n, bodies, &error);
+        ocl_err(error);
+        ocl_err(clFinish(g_command_queue));
+
+        // Set kernel arguments
+        cl_uint arg_num = 0;
+        ocl_err(clSetKernelArg(kernel, arg_num++, sizeof(n), &n));
+        ocl_err(clSetKernelArg(kernel, arg_num++, sizeof(dev_bodies), &dev_bodies));
+        ocl_err(clFinish(g_command_queue));
+    }
 
     CallbackData cd = {
             kernel,                 /* cl_kernel kernel */
@@ -132,7 +296,8 @@ int main(int argc, char ** argv)
             {0.7, 0.7, 1, 1},       /* Vect4f point_color */
             2,                      /* float point_size */
             true,                   /* bool draw_lines */
-            {0.7, 0.7, 0.7, 0.2}    /* Vect4f line_color */
+            {0.7, 0.7, 0.7, 0.2},   /* Vect4f line_color */
+            mode,                   /* Mode */
     };
 
     renderer_start(&cd, step, draw);
